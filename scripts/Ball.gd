1d5ac430a494ce59095c54fe35c10d6b54e0fb89
Luke <lucasromerodb@gmail.com>
2023-02-19 22:00:40 -0300

    ball physics, instances and socre

--- scripts/Ball.gd
+++ scripts/Ball.gd
@@ -1,12 +1,26 @@
 extends RigidBody2D
 
+export var speedup = 10
+const MAXSPEED = 400
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_physics_process(true)
 	
-func _physics_process(delta):
+func _physics_process(_delta):
 	var bodies = get_colliding_bodies()
 	
 	for body in bodies:
 		if body.is_in_group("Bricks"):
+			get_node("/root/World").score += 5
 			body.queue_free()
+		
+		if body.get_name() == "Paddle":
+			var speed = get_linear_velocity().length()
+			var direction = get_position() - body.get_node("Anchor").get_global_position()
+			var velocity = direction.normalized()*min(speed+MAXSPEED, MAXSPEED)
+			set_linear_velocity(velocity)
+	
+	if get_position().y > get_viewport_rect().end.y:
+		print("Ball died.")
+		queue_free()
--- scripts/Paddle.gd
+++ scripts/Paddle.gd
@@ -1,11 +1,20 @@
 extends KinematicBody2D
 
+const ball_scene = preload("res://components/Ball.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_physics_process(true)
+	set_process_input(true)
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var y = position.y
 	var mouse_x = get_viewport().get_mouse_position().x
 	set_position(Vector2(mouse_x, y))
+
+func _input(event):
+	if event is InputEventMouseButton and event.is_pressed():
+		var ball = ball_scene.instance()
+		ball.set_position(get_position()-Vector2(0, 16))
+		get_tree().get_root().add_child(ball)
 
